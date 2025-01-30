5f742818b1cac408caba38c727a41ca4942213af
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-30 18:49:04 +0530

    Update JSXRules.jsx

--- Basics/JSXRules/JSXRules.jsx
+++ Basics/JSXRules/JSXRules.jsx
@@ -1,14 +1,14 @@
-import JSXRules from "./components/JSXRules";
 
-const App=()=>
-{
-  
 
+const JSXRules = () => {
   return (
-
-    <JSXRules/>
+    <div>
+    <h1>JSX Must return single Parent Element</h1>
+    <h2>All tags Must be Closed </h2>
+    <h3>For declaring Class used ClassName=""</h3>
+    <h4>when label declared ,we see a for in html where as in jSX we need to declare it as htmlFor</h4>
+    </div>
   )
-
 }
 
-export default App;
+export default JSXRules
