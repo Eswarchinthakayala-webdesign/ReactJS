4e981cd6b3c62013b8c5508e7888d3f9f0d3b67d
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-01-30 18:32:01 +0530

    Create App.jsx

---
+++ Basics/Create ID and Class/App.jsx
@@ -0,0 +1,19 @@
+
+
+const App=()=>
+{
+  
+
+  return (
+
+    <div>
+      <h1>What is JSX</h1>
+      <div id="id">JavaScript XML</div>
+      <p className="text">Component</p>
+    </div>
+    
+  )
+
+}
+
+export default App;
