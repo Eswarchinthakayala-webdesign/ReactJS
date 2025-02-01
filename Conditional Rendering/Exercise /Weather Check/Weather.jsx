922d5dc53f535be7f1f02f6d0011de7abbbb8ba6
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-01 19:08:37 +0530

    Create Weather.jsx

---
+++ Conditional Rendering/Exercise /Weather Check/Weather.jsx
@@ -0,0 +1,18 @@
+
+const Weather = ({temperature}) => {
+  
+    if(temperature<15)
+    {
+        return <h1>It's Cold Outside</h1>
+    }
+    else if(temperature<25)
+    {
+        return <h1>It's nice Outside</h1>
+    }
+    else
+    {
+        return <h1>It's hot outside</h1>
+    }
+}
+
+export default Weather
