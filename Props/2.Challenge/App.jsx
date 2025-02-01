48336c74827e4c0a5c541766e1133385da20d7c7
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-01 18:28:05 +0530

    Create App.jsx

---
+++ Props/2.Challenge/App.jsx
@@ -0,0 +1,13 @@
+import  Card from './components/Card.jsx'
+export const App = () => {
+  return (
+   <div>
+    <Card>
+      <h1>React js </h1>
+      <h2>Is fun to learn</h2>
+    </Card>
+   </div>
+  )
+}
+
+export default App
