ec422e99ce9ba6d07c0e1722a5964c5ed7226cc3
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-07 19:09:33 +0530

    Create App.jsx

---
+++ Hooks/UseState/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+
+
+const App = () => {
+  const [count,setCount]=useState(0);
+ 
+  return (
+    <div>
+     <p>Count:{count}</p>
+     <button onClick={()=>setCount(count+1)}>Increment</button>
+    </div>
+  )
+}
+
+export default App
