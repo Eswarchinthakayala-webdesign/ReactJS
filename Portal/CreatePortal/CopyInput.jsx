af6cd0769e52cef4283728c0be9f0b7a3af0597b
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-11 21:19:37 +0530

    Create CopyInput.jsx

---
+++ Portal/CreatePortal/CopyInput.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import PopUp from "./PopUp";
+
+const CopyInput = () => {
+
+
+    const[inputValue,setInputValue]=useState('');
+    const[copied,setCopied]=useState(false)
+    const handleCopy=()=>
+    {
+        navigator.clipboard.writeText(inputValue).then(()=>
+        {
+        setCopied(true)
+           setTimeout(()=>{
+            setCopied(false)
+           },2000)
+        })
+    }
+
+  return (
+    <div>
+
+   <input type="text" value={inputValue}  onChange={e=>setInputValue(e.target.value)} />
+
+    <button onClick={handleCopy}>Copy </button>
+    <PopUp copied={copied}/>
+    </div>
+  )
+}
+
+export default CopyInput
