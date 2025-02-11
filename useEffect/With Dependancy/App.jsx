8b3e0841c0e85a728c10aa71adae775858a0a799
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-11 21:57:01 +0530

    Create App.jsx

---
+++ useEffect/With Dependancy/App.jsx
@@ -0,0 +1,26 @@
+
+import { useState,useEffect } from "react"
+const App = () => {
+  const [value,setValue]=useState(0)
+
+  const [v,setV]=useState(0);
+  useEffect(()=>
+  {
+    console.log("Counter 2",v)
+  },[v])
+  useEffect(()=>
+  {
+   console.log("Counter 1",value)
+   document.title=`Use Effect ${value}`
+},[value])
+  return (
+    <div>
+      <p>Counter 1:{value}</p>
+      <button onClick={()=>setValue(s=>s+1)}>Increment 1</button>
+      <p>Counter 2:{v}</p>
+      <button onClick={()=>setV(s=>s+1)}>Increment 2</button>
+    </div>
+  )
+}
+
+export default App
