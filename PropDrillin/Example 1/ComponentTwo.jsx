01e3548aace129968b0613718282e05227aa89e6
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-02-12 17:08:17 +0530

    Create ComponentTwo.jsx

---
+++ PropDrillin/Example 1/ComponentTwo.jsx
@@ -0,0 +1,12 @@
+import ComponentC from "./ComponentC"
+
+const ComponentB = ({name}) => {
+
+  return (
+    <div>
+        <ComponentC name={name}/>
+    </div>
+  )
+}
+
+export default ComponentB
