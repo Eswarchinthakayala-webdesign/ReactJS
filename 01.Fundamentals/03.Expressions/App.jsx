0b4f1b1217a2cbe4ec088906135fa948ec73bb3d
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-07-23 12:47:11 +0530

    Update App.jsx

--- 01.Fundamentals/03.Expressions/App.jsx
+++ 01.Fundamentals/03.Expressions/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const App = () => {
+
+    const myName="Eswar"
+    const unRead=3
+    const isLogged=true
   return (
    <>
    
    {2+2}
+   <h1>{myName}</h1>
+   {unRead>2 &&<p> You have {unRead} Messages</p> }
+   {isLogged ? "Welcome Back":"Please Sign in"}
    
    </>
   )
