b6cbfed914e537dd45eaee282e56c7430a72cdbf
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-07-23 12:25:35 +0530

    Create App.jsx

---
+++ 01.Fundamentals/02.JSX/Coding/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+   <section id="section">
+    <h1>My Website</h1>
+    <article>
+      <h2>Welcome to React</h2>
+      <p className="text">Paragraph Content</p>
+    </article>
+   </section>
+  )
+}
+
+export default App
